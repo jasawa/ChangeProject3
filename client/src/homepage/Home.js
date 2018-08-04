a5f3aaf24a493b96bd76ddd0e81b11371421b293
Jane Asawa <asawajane@gmail.com>
2018-08-04 10:31:01 -0700

    added homepage2

--- client/src/homepage/Home.js
+++ client/src/homepage/Home.js
@@ -1,12 +1,58 @@
 import React, { Component } from 'react';
 //import Nav from '../components/Nav'
-
+//import { Col, Row, Container } from "../components/Grid";
 //require('./Home.css');
 
-const Home = () =>
-    <div>
-        <h1>Home page</h1>
+export default class Home extends Component {
+    render() {
+        return (
+            <div>
+            <section className="jumbotron" ></section>
+            <div className="container mt-4 mb-4">
+                <div className="row bg-warning text-center">
+                    <div className="col col-md-3 col-sm-12"></div>
+                    <div className="col col-md-6 col-sm-12">
 
-    </div>
+                        <img src={require("./images/lg-Hopics.png")} className="img-fluid" alt="HOPICS logo" />
 
-export default Home;
+                    </div>
+                    <div className="col col-md-3 col-sm-12"></div>
+                </div>
+            </div>
+            <section className="jumbotron" ></section>
+
+            <div className="container-fluid mb-4">
+                <div className="row bg-warning text-center">
+                    <div className="col col-md-3 col-sm-12">
+                         <img src={require("./images/lg-Hopics.png")} className="img-fluid" alt="HOPICS logo" />
+                    </div>
+                    <div className="col col-md-6 col-sm-12"></div>
+                    <div className="col col-md-3 col-sm-12">
+                        <img src={require("./images/lg-Hopics.png")} className="img-fluid" alt="HOPICS logo" />
+                    </div>
+                </div>
+            </div>
+
+
+
+            <section className="jumbotron" ></section>
+            <div className="container mt-4 mb-4">
+                <div className="row bg-warning text-center">
+                    <div className="col col-md-3 col-sm-12"></div>
+                    <div className="col col-md-6 col-sm-12">
+
+                        <img src={require("./images/lg-Hopics.png")} className="img-fluid" alt="HOPICS logo" />
+
+                    </div>
+                    <div className="col col-md-3 col-sm-12"></div>
+                </div>
+            </div>
+            <section className="jumbotron" ></section>
+
+
+
+
+            </div>
+        )
+    }
+}
